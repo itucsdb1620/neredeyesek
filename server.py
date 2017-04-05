b6386c3a35a89142d18d46bf412ab1f0604aa97f
BerkanDinar <berkandinar@hotmail.com>
2017-04-05 03:42:20 +0300

    Update server.py

--- server.py
+++ server.py
@@ -10,6 +10,10 @@ from flask.globals import session
 def home_page():
     return render_template('home.html')
 
+@app.route('/main',  methods=['GET','POST'])
+def main_page():
+    do_work()
+    return redirect(url_for('home_page'))
 @app.route('/login',  methods=['POST'])
 def login_page():
     username = request.form['username']
